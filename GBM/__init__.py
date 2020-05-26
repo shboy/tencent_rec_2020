486bf6f7345b37faaef04cff686128a9c3fd9408
shenhao <shenhao@xiaomi.com>
2020-05-26 23:04:06 +0800

    update

---
+++ GBM/__init__.py
@@ -0,0 +1,6 @@
+'''
+@Time    : 2020/5/26 21:46
+@Author  : sh_lord
+@FileName: __init__.py.py
+
+'''
---
+++ GBM_MODEL.py
@@ -0,0 +1,11 @@
+'''
+@Time    : 2020/5/26 21:45
+@Author  : sh_lord
+@FileName: GBM_MODEL.py
+
+'''
+class GBM_MODEL:
+    def __init__(self):
+        self.user_train = "train_preliminary/user.sample.csv"
+        self.ad_train = "train_preliminary/ad.sample.csv"
+        self.click_log_train = "train_preliminary/click_log.sample.csv"
