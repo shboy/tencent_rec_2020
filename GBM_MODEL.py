7c448a252e9ec2ed027adad9a26203d3121ffa67
shenhao <shenhao@xiaomi.com>
2020-05-27 13:31:51 +0800

    update

--- GBM_MODEL.py
+++ GBM_MODEL.py
@@ -15,11 +15,16 @@ class GBM_MODEL:
         self.user_train = "train_preliminary/user.sample.csv"
         self.ad_train = "train_preliminary/ad.sample.csv"
         self.click_log_train = "train_preliminary/click_log.sample.csv"
-        
-    if __name__ == '__main__':
+
+
+    def train(self):
         target = 'Disbursed'
         IDcol = 'user_id'
         # Choose all predictors except target & IDcols
         predictors = [x for x in self.user_train.columns if x not in [target, IDcol]]
         gbm0 = GradientBoostingClassifier(random_state=10)
+        # 多分类：
+        # https://zhuanlan.zhihu.com/p/91652813?utm_source=wechat_session
+        # https://www.2cto.com/kf/201802/717234.html
+        # https://www.jianshu.com/p/516f009c0875
         modelfit(gbm0, train, test, predictors, printOOB=False)
--- data_preparation.py
+++ data_preparation.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
+FIELD_SEP = '_'
 
 class Data_Preparation:
     # user: Index(['user_id', 'age', 'gender'], dtype='object'),
@@ -20,13 +21,27 @@ class Data_Preparation:
         # print(self.ad_train.columns)
         self.click_log_train = pd.read_csv("train_preliminary/click_log.sample.csv")
         # print(self.click_log_train.columns)
-        self.df_train = self.data_process()
+        self.label_dict = self.gen_label_dict()
+        # self.df_train = self.data_process()
+
+    def gen_label_dict(self):
+        label_dict = {}
+        label = 0
+        for age in range(1, 11):
+            for gender in range(1, 3):
+                label_dict[str(age) + FIELD_SEP + str(gender)] = label
+                label += 1
+        return label_dict
 
     def data_process(self):
         df_train = self.user_train.join(self.click_log_train.set_index('user_id'), on='user_id', how='inner')
         df_train = df_train.join(self.ad_train.set_index('creative_id'), on='creative_id', how='inner')
         print(df_train.head(5))
         print(df_train.dtypes)
+        df_train['label'] = (df_train['age'].map(str) + FIELD_SEP + df_train['gender'].map(str)).map(self.label_dict.get)
+        df_train.drop(['age', 'gender'], axis=1, inplace=True)
+        print("label:\t", df_train['label'].values)
+        # 合并age、 gender
         # Numerical Coding:
         le = LabelEncoder()
         var_to_encode = ['product_id', 'industry']
@@ -35,17 +50,13 @@ class Data_Preparation:
         # One-Hot Coding
         df_train = pd.get_dummies(df_train, columns=var_to_encode)
         for idx in df_train.columns:
-            print(idx, ", ")
+            print(idx, end=", ")
+        print()
         # print(df_train.columns)
         df_train.to_csv('train_modified.csv', index=False)
         return df_train
-
-    def train(self):
-        target = 'Disbursed'
-        IDcol = 'ID'
 
 
 if __name__ == '__main__':
     dp = Data_Preparation()
     dp.data_process()
-
