5f1b80ed5d4e87acba5b73471195a54c48f4d3af
shenhao <shenhao@xiaomi.com>
2020-05-31 21:55:39 +0800

    update

--- data_preparation.py
+++ data_preparation.py
@@ -89,7 +89,10 @@ class Data_Preparation:
         gc.collect()
 
         # 合并age、 gender
+        print("生成label开始")
         df_train['label'] = (df_train['age'].map(str) + FIELD_SEP + df_train['gender'].map(str)).map(self.label_dict.get)
+        print("生成label完成")
+
         df_train.drop(['age', 'gender'], axis=1, inplace=True)
         print("label:\t", df_train['label'].values)
         var_to_encode = ['product_id', 'industry']
